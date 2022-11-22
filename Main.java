56cd17bef51a3fb4a813677d4fd6bc8e9baddee0
avis97 <106426358+avis97@users.noreply.github.com>
2022-11-22 18:12:56 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-
-package com.mygame;
-public class Main{
-    public static void main(String [] args){
-        System.out.println("My first program.");
-        Mygame game=new Mygame();
-    }
-}
